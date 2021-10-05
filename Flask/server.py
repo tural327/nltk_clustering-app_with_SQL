bc7941af7950081572b29986493b7613e2821606
Tural <56700191+tural327@users.noreply.github.com>
2021-10-05 23:03:41 +0400

    changed pswrd

--- Flask/server.py
+++ Flask/server.py
@@ -8,7 +8,7 @@ def add_server(msg,time,group):
     connection = sql.connect(
     host = "127.0.0.1",
     user="root",
-    password='tural123',
+    password='add your code',
     auth_plugin = "mysql_native_password"
     
     )
